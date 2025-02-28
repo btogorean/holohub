5fe827a7f94e5f37ed006869993d471ff850c4e0
Bogdan Togorean <bogdan.togorean@analog.com>
2025-02-28 09:29:52 +0200

    Initial commit of matlab_classify_modulator_adv
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

---
+++ applications/matlab_classify_modulator_adv/cpp/matlab/classifyModulation.m
@@ -0,0 +1,52 @@
+function [v, modulation] = classifyModulation(inputSig, chan) %#codegen
+assert(isa(inputSig,'single') && ~isreal(inputSig) && all(size(inputSig) == [4096,1]));
+assert(isa(chan, 'uint8') && isreal(chan) && all(size(chan) == [1,1]), ...
+    'Channel number must be an integer between 1 and 4.');
+
+coder.gpu.kernelfun();
+% input signal size is 1024-by-2
+
+% parameters
+ModelFile = 'mod_classify_model.mat'; % file that saves the neural network model
+% imSize = [1024 1]; % Size of the input image for the deep learning network
+
+%Function to converts signal to wavelet time-frequency image
+% im = cwtModType(inputSig, imSize);
+
+%Load the trained deep learning network
+persistent model;
+if isempty(model)
+    model = coder.loadDeepLearningNetwork(ModelFile, 'mynet');
+end
+
+%Changes CJ
+selectedChannel = complex(zeros(1024, 1, 'single')); % Preallocate for fixed size
+index = 1;
+for i = double(chan):double(4):double(4096)
+    selectedChannel(index) = inputSig(i);
+    index = index + 1;
+end
+framePower = mean(abs(selectedChannel).^2);
+selectedChannel = selectedChannel / sqrt(framePower);
+%End Changes
+
+inputSigI = real(selectedChannel);
+inputSigQ = imag(selectedChannel);
+inputSigIQ = [inputSigI, inputSigQ];
+
+% Predict the Signal Modulation
+ldIn = dlarray(inputSigIQ,"TC");
+dlA = model.predict(ldIn);
+predClassProb = extractdata(dlA);
+% predClassProb = model.predict(inputSig);
+
+[v,i] = max(predClassProb);
+% disp(predClassProb)
+% disp(i)
+%mods = ["16QAM", "64QAM", "8PSK", "BPSK", "CPFSK", "GFSK", "PAM4", "QPSK"];
+% mods = ["16QAM", "64QAM", "8PSK", "B-FM", "BPSK", "CPFSK", "DSB-AM", "GFSK", "PAM4", "QPSK", "SSB-AM"];
+% modulation = mods(i);
+modulation=i;
+
+
+end
