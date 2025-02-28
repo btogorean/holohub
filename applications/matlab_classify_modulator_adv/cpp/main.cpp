28b11b5cedf7b872933b3671b912cd67788b3c5a
Bogdan Togorean <bogdan.togorean@analog.com>
2025-02-28 09:29:52 +0200

    Initial commit of matlab_classify_modulator_adv
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

---
+++ applications/matlab_classify_modulator_adv/cpp/main.cpp
@@ -0,0 +1,98 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 ADI Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "default_op_rx.h"
+#include "adv_network_kernels.h"
+#include <holoscan/holoscan.hpp>
+
+#include "classifyModulation.h"
+#include "classifyModulation_terminate.h"
+
+namespace holoscan::ops {
+class MatlabClassifyModulationOp : public Operator {
+ public:
+  HOLOSCAN_OPERATOR_FORWARD_ARGS(MatlabClassifyModulationOp)
+
+  MatlabClassifyModulationOp() = default;
+
+  void setup(OperatorSpec& spec) override {
+    spec.input<std::shared_ptr<AdvNetBurstParams>>("burst_in");
+  }
+
+  void start() {
+  }
+
+  void stop() {
+    classifyModulation_terminate();
+  }
+
+  void compute(InputContext& op_input, OutputContext& op_output,
+               ExecutionContext& context) override {
+    float v;
+    double modulation;
+
+    creal32_T val[1024] = {};
+
+    // Call MATLAB CUDA function to do modulation classification
+    classifyModulation(val, &v, &modulation);
+
+    // Create output message
+    HOLOSCAN_LOG_INFO("Confidence: {}", v);
+    HOLOSCAN_LOG_INFO("Modulation: {}", modulation);
+
+    //auto result = modulation;
+    //op_output.emit(result);
+  }
+
+ private:
+};
+
+}  // namespace holoscan::ops
+
+class MatlabClassifyModulationApp : public holoscan::Application {
+ public:
+  void compose() override {
+    using namespace holoscan;
+
+    auto output_rx_ports = holoscan::ops::adv_net_get_port_names(config(), "rx");
+    auto adv_net_rx =
+      make_operator<ops::AdvNetworkOpRx>("adv_network_rx", output_rx_ports, from_config("advanced_network"),
+                                          make_condition<BooleanCondition>("is_alive", true));
+
+    // Define operators and configure using yaml configuration
+    auto matlab = make_operator<ops::MatlabClassifyModulationOp>("matlab", make_condition<CountCondition>(-1));
+
+    // Define the workflow
+    add_flow(adv_net_rx, matlab, {{"adv_rx_out", "burst_in"}});
+  }
+};
+
+int main(int argc, char** argv) {
+  // Get the yaml configuration file
+  auto config_path = std::filesystem::canonical(argv[0]).parent_path();
+  config_path /= std::filesystem::path("matlab_classify_modulator_adv.yaml");
+  if (argc >= 2) { config_path = argv[1]; }
+
+  auto app = holoscan::make_application<MatlabClassifyModulationApp>();
+  app->config(config_path);
+  app->scheduler(app->make_scheduler<holoscan::MultiThreadScheduler>(
+      "multithread-scheduler", app->from_config("scheduler")));
+  app->run();
+
+  holoscan::ops::adv_net_shutdown();
+  return 0;
+}
---
+++ applications/matlab_classify_modulator_adv/cpp/matlab/gen_model_file_x86.m
@@ -0,0 +1,88 @@
+%% Generate model for classification function
+matfile = 'mod_classify_model.mat';
+% load trainedModulationClassificationNetwork
+load mod_classify_model_split_iq.mat
+save(matfile,'trainedNet');
+
+%% Test MATLAB
+modulationTypes = categorical(sort(["BPSK", "QPSK", "8PSK", ...
+  "16QAM", "64QAM", "PAM4", "GFSK", "CPFSK"]));
+
+
+for mode = 1:length(modulationTypes)
+    data = load(['mod_',char(modulationTypes(mode)),'.mat']);
+
+    data.rx = repelem(data.rx, 4);
+    data1.rx = single(data.rx);
+
+    [prob, result] = classifyModulation(data1.rx, uint8(1));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(2));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(3));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(4));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+end
+
+%% Gen Code mex
+cfg = coder.gpuConfig('mex');
+cfg.GpuConfig.CompilerFlags = '--fmad=false';
+cfg.GenerateReport = true;
+% cfg.DeepLearningConfig = coder.DeepLearningConfig('cudnn');
+cfg.DeepLearningConfig = coder.DeepLearningConfig('none');
+% cfg.DeepLearningConfig.AutoTuning = true;
+codegen -config cfg classifyModulation
+
+%% Gen Code dll
+cfg = coder.gpuConfig('dll','ecoder',true);
+cfg.GpuConfig.CompilerFlags = '--fmad=false';
+cfg.GenerateReport = true;
+% cfg.DeepLearningConfig = coder.DeepLearningConfig('cudnn');
+cfg.DeepLearningConfig = coder.DeepLearningConfig('none');
+% cfg.DeepLearningConfig.AutoTuning = true;
+codegen -config cfg classifyModulation
+
+
+%% Test
+for k  = 1
+for mode = 1:length(modulationTypes)
+    data = load(['mod_',char(modulationTypes(mode)),'.mat']);
+    data.rx = repelem(data.rx, 4);
+    data1.rx = single(data.rx)
+
+    [prob, result] = classifyModulation_mex(data1.rx, uint8(1));
+
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+end
+end
+
+% disp('REAL DATA');
+% data = load(['osc_capt.mat']);
+% data1 = complex(single(data.axi_adrv9009_rx_hpc_voltage0_i), single(data.axi_adrv9009_rx_hpc_voltage0_q));
+% 
+% [prob, result] = classifyModulation_mex(data1);
+% 
+% disp(prob);
+% disp(result);
+% disp('------');
