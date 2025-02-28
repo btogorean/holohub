c19e42b7fcd3a3563db053256d660427090cec51
Bogdan Togorean <bogdan.togorean@analog.com>
2025-02-28 09:29:52 +0200

    Initial commit of matlab_classify_modulator_adv
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

---
+++ applications/matlab_classify_modulator_adv/cpp/matlab/gen_model_file_arm64.m
@@ -0,0 +1,56 @@
+% Specify IP, username and p/w of device
+clear hwobj
+hwobj = jetson('10.48.65.198','analog','analog');
+
+%% Generate model for classification function
+matfile = 'mod_classify_model.mat';
+% load trainedModulationClassificationNetwork
+load mod_classify_model_split_iq.mat
+save(matfile,'trainedNet');
+
+%% Test MATLAB
+modulationTypes = categorical(sort(["BPSK", "QPSK", "8PSK", ...
+  "16QAM", "64QAM", "PAM4", "GFSK", "CPFSK"]));
+
+
+for mode = 1:length(modulationTypes)
+    data = load(['mod_',char(modulationTypes(mode)),'.mat']);
+
+    data.rx = repelem(data.rx, 4);
+    data1.rx = single(data.rx);
+
+    [prob, result] = classifyModulation(data1.rx, uint8(1));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(2));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(3));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+    [prob, result] = classifyModulation(data1.rx, uint8(4));
+    disp(prob);
+    disp(result);
+    disp(modulationTypes(mode));
+    disp('------');
+
+end
+
+%% Gen Code dll
+cfg = coder.gpuConfig('dll','ecoder',true);
+cfg.GpuConfig.CompilerFlags = '--fmad=false';
+cfg.GenerateReport = true;
+% cfg.DeepLearningConfig = coder.DeepLearningConfig('cudnn');
+cfg.DeepLearningConfig = coder.DeepLearningConfig('none');
+% cfg.DeepLearningConfig.AutoTuning = true;
+cfg.Hardware = coder.hardware('NVIDIA Jetson');
+codegen -config cfg classifyModulation
